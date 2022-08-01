b326d18c27b1bdda063b214e1101f8b138613400
ffabre87 <fabrefacundo@gmail.com>
2022-07-31 22:09:50 -0300

    actualizacion foto perfil y datos

---
+++ AngularFabre/BackEnd/src/main/java/com/portfolio/ffabre/FfabreApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.ffabre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FfabreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FfabreApplication.class, args);
+	}
+
+}
---
+++ AngularFabre/BackEnd/src/test/java/com/portfolio/ffabre/FfabreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.ffabre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FfabreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
